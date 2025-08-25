76adc523853a71e57dd6e8b12e03fe2fd84ba137
renrennn27 <renoalifprasetyo20@gmail.com>
2025-08-25 10:24:12 +0700

    menambahkan file

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.writeln('<h1>wenak wenakk wenak</h1>')
+// document.writeln(100)
+// console.log
+var a = "rudii"
+var b = "hajat"
+var c = a + b
+console.log(c)
+
+let d = 1000
+console.log(d)
+d = 3000
+console.log(d)
+
+function pembagian (x, y) {
+    return x / y
+}
+console.log(pembagian(4, 2))
